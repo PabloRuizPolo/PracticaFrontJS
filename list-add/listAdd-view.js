3279f431f90fd6590b589ec9493dd61a155bde2a
Pablo Ruiz <pablo.ruiz.polo@gmail.com>
2024-03-18 13:12:03 +0100

    Recoto un peuqueño fallo de la view. Hay que darle forma

--- list-add/listAdd-view.js
+++ list-add/listAdd-view.js
@@ -3,9 +3,9 @@
 export function showAdds(add) {
     return `
     <p>${add.nombre}</p>
-    <p>${add.venta}</p>
-    <img>${add.foto}</img>
     <p>${add.foto ? 'Se vende' : 'Se compra'}</p>
+    <img>${add.foto}</img>
+    <p>${add.precio}</p>
     <p>${add.descripcion}</p>
     `
 }
